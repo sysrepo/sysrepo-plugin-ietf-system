73c805e2c4385f583ce05c09389e64dc1538ae52
Jan Kundrát <jan.kundrat@cesnet.cz>
2016-11-30 00:55:29 +0100

    Support setting the time zone name through systemd

--- system-plugin.c
+++ system-plugin.c
@@ -1,11 +1,19 @@
 #include <assert.h>
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/utsname.h>
+#include <sys/wait.h>
 #include <syslog.h>
 #include <sysrepo.h>
 #include <time.h>
 #include <unistd.h>
+
+typedef void (*sighandler_t)(int);
 
 // Next:
 // ? Timezone
@@ -16,8 +24,66 @@
 //   Set datetime RPC
 //   Restart and shutdown RPCs (done)
 
+
+int exec_wrapper_with_args(char * const args[])
+{
+    /* We have to ensure that SIGCHLD handler is not SIG_IGN; if it is a SIG_IGN, then waitpid() won't work */
+    sighandler_t orig_sig_chld = signal(SIGCHLD, SIG_DFL);
+    pid_t pid = fork();
+    if (pid == -1) {
+        syslog(LOG_ERR, "fork failed");
+        signal(SIGCHLD, orig_sig_chld);
+        return SR_ERR_INTERNAL;
+    } else if (pid == 0) {
+        execv(args[0], args);
+        exit(EXIT_FAILURE);
+    } else {
+        int wstatus;
+        while (waitpid(pid, &wstatus, 0) == -1) {
+            if (errno == EINTR)
+                continue;
+            signal(SIGCHLD, orig_sig_chld);
+            syslog(LOG_ERR, "waitpid error: %d", errno);
+            return SR_ERR_INTERNAL;
+        }
+        signal(SIGCHLD, orig_sig_chld);
+        if (WIFSIGNALED(wstatus)) {
+            int res = WTERMSIG(wstatus);
+            syslog(LOG_ERR, "%s killed by signal %d", args[0], res);
+            return SR_ERR_INTERNAL;
+        }
+        int exit_status = WEXITSTATUS(wstatus);
+        if (!WIFEXITED(wstatus)) {
+            syslog(LOG_ERR, "%s died, exit status %d", args[0], exit_status);
+            return SR_ERR_INTERNAL;
+        }
+        if (exit_status) {
+            syslog(LOG_ERR, "%s exit status %d", args[0], exit_status);
+            return SR_ERR_INTERNAL;
+        }
+        return SR_ERR_OK;
+    }
+}
+
+static const char * sr_event_to_string(const sr_notif_event_t event)
+{
+    switch (event) {
+    case SR_EV_ABORT:
+        return "abort";
+    case SR_EV_APPLY:
+        return "apply";
+    case SR_EV_ENABLED:
+        return "enabled";
+    case SR_EV_VERIFY:
+        return "verify";
+    }
+    assert(false);
+    return "unknown";
+}
+
 int hostname_cb(sr_session_ctx_t *session, const char *xpath, sr_notif_event_t event, void *private_ctx)
 {
+    syslog(LOG_DEBUG, "%s: event %s, xpath %s", __func__, sr_event_to_string(event), xpath);
     if (event == SR_EV_ABORT) {
         return SR_ERR_OK;
     }
@@ -151,12 +217,66 @@ int exec_rpc_cb(const char* xpath, const sr_val_t* input, const size_t input_cnt
     return SR_ERR_OK;
 }
 
+#define TIMEDATECTL_BIN "/usr/bin/timedatectl"
+
+int timezone_name_cb(sr_session_ctx_t *session, const char *xpath, sr_notif_event_t event, void *private_ctx)
+{
+    syslog(LOG_DEBUG, "%s: event %s, xpath %s", __func__, sr_event_to_string(event), xpath);
+    if (event == SR_EV_ABORT) {
+        return SR_ERR_OK;
+    }
+
+    sr_val_t* value = NULL;
+    int rc = SR_ERR_OK;
+    const char *timezone = NULL;
+    rc = sr_get_item(session, "/ietf-system:system/clock/timezone-name", &value);
+    if (SR_ERR_NOT_FOUND == rc) {
+        timezone = "UTC";
+    } else if (SR_ERR_OK != rc) {
+        syslog(LOG_DEBUG, "Error retrieving timezone-name: %s", sr_strerror(rc));
+        return rc;
+    } else {
+        assert(value->type == SR_STRING_T);
+        timezone = value->data.string_val;
+    }
+
+    if (event == SR_EV_VERIFY) {
+        char fname[PATH_MAX+1];
+        snprintf(fname, sizeof(fname), "/usr/share/zoneinfo/%s", timezone);
+        if (access(fname, F_OK)) {
+            syslog(LOG_INFO, "Rejecting invalid timezone %s", timezone);
+            rc = SR_ERR_VALIDATION_FAILED;
+            sr_set_error(session, "Requested timezone not found", xpath);
+            goto cleanup;
+        }
+        if (access(TIMEDATECTL_BIN, X_OK)) {
+            syslog(LOG_ERR, TIMEDATECTL_BIN " not available");
+            rc = SR_ERR_VALIDATION_FAILED;
+            sr_set_error(session, TIMEDATECTL_BIN " not available", xpath);
+            goto cleanup;
+        }
+        rc = SR_ERR_OK;
+    } else {
+        syslog(LOG_DEBUG, "Setting timezone to %s", timezone);
+        char * args[] = {TIMEDATECTL_BIN, "set-timezone", (char *)timezone, NULL};
+        rc = exec_wrapper_with_args(args);
+    }
+cleanup:
+    sr_free_val(value);
+    return rc;
+}
+
 int sr_plugin_init_cb(sr_session_ctx_t* session, void** private_ctx)
 {
     sr_subscription_ctx_t* subscription = NULL;
     int rc = SR_ERR_OK;
 
     rc = sr_subtree_change_subscribe(session, "/ietf-system:system/hostname", hostname_cb, NULL, 0,
+                                     SR_SUBSCR_CTX_REUSE | SR_SUBSCR_EV_ENABLED, &subscription);
+    if (SR_ERR_OK != rc)
+        goto error;
+
+    rc = sr_subtree_change_subscribe(session, "/ietf-system:system/clock/timezone-name", timezone_name_cb, NULL, 0,
                                      SR_SUBSCR_CTX_REUSE | SR_SUBSCR_EV_ENABLED, &subscription);
     if (SR_ERR_OK != rc)
         goto error;
